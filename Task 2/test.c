4a1a51e4d0ffd2c3b359862ca0c6c99fab367bc1
AmoghMoses <amoghmoses2002@gmail.com>
2023-05-01 12:38:34 +0530

    Task 1 and 2 working, need to remove plag

--- Task 2/test.c
+++ Task 2/test.c
@@ -4,7 +4,7 @@ int main()
     int i = 20;
     int var1 = 9;
     int arr[6] = {1,23,3};
-    // int arr[2][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}};
+    int arr2[2][5] = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}};
     int arr1[5] = {1, 2, 3, 4, 5};
     while(i < 5)
     {
@@ -15,7 +15,8 @@ int main()
         if(i == 4)
             break;
     }
-
+    int x = arr2[i][4];
+    int y = arr2[arr1[0]][arr2[0][0]];
     printf("hello world\n");
 }
     /*
--- Task 3/test.c
+++ Task 3/test.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 void main()
 {
-    int n,i;
+    int n, i;
     int arr[3] = {1,2,3};
     // arr[100] = 3;
-    printf("%d\n",arr[1]);
+    printf("%d\n",arr[i]);
 }
